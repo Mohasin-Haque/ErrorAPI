1cee5c553e5844d38a84f12371db1d6734a1d0ea
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-10-05 06:39:40 +0000

    added error handling message

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const outputDiv = document.querySelector("#output");
 var text = "Aainda se mere khilaf bagawat ki..........KAT DALUNGA";
 var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text;
 
-function showError(){
-  cosnsole.log("Bhai bhai bhut khtrnak error de rha h yar.. y eto bilkul bhr bhr ke error de rha h..")
+function showError(error){
+  cosnsole.log("Bhai bhai bhut khtrnak error de rha h yar.. y eto bilkul bhr bhr ke error de rha h.."+ error)
+  if(error === 404){
+    consolr.log("Page not found");
+  }else if(error === 401){
+    console.log("please log in to continue");
+  }
 }
 
 function errorHandler(){
