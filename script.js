aaabefb1b73eeb91800a9d5d5277c87a641d28bd
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-11-09 13:13:07 +0530

    fix: api and code

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const getAPI = document.querySelector("#btn");
 const outputDiv = document.querySelector("#output");
-var text = "Aainda se mere khilaf bagawat ki..........KAT DALUNGA";
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text;
 
-function showError(error){
-  cosnsole.log("Bhai bhai bhut khtrnak error de rha h yar.. y eto bilkul bhr bhr ke error de rha h.."+ error)
-  if(error === 404){
-    consolr.log("Page not found");
-  }else if(error === 401){
-    console.log("please log in to continue");
-  }
-}
+var url = "https://unitube-server.herokuapp.com/playlists"
 
 function errorHandler(){
-fetch(url).then(response => response.json()).then(json => outputDiv.innerHTML = json.contents.text).catch(showError);
+fetch(url).then(response => {
+  if(error === 404){
+    outputDiv.innnerText = "page not found"
+  }else if(error === 401){
+    outputDiv.innerText = "please log in to continue"
+  }else {
+    return response.json();
+  }
+});
 }
 
 getAPI.addEventListener("click", errorHandler)
